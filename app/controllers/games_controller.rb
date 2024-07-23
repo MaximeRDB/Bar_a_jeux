97d50ac3335e63ae115d4ea0d84cff9cbd001791
Maxime Rollinde de Beaumont <maximerdb@hotmail.fr>
2024-07-23 17:45:29 +0200

    creation of a controller Bars
    Signed-off-by: Maxime Rollinde de Beaumont <maximerdb@hotmail.fr>

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,9 +2,9 @@ class GamesController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update, :destroy]
   before_action :game_params, only: [:create]
 
-def index
-  @games = Game.all
-end
+  def index
+    @games = Game.all
+  end
 
   def show
   end
@@ -20,7 +20,7 @@ end
     if game_params[:barcode].present?
      @game = Games::Creator.new(game_params[:barcode]).call
     else
-      @game = Game.new(game_params)
+      @game = Game.new(game_params.compact!)
     end
 
     if @game.save!
@@ -33,13 +33,18 @@ end
   def update
     respond_to do |format|
       if @game.update!(
-        description: game_params[:description],
-        name: game_params[:name],
-        image_link: game_params[:image_link])
+        description: game_params[:description].presence,
+        name: game_params[:name].presence,
+        image_link: game_params[:image_link].presence)
 
           format.html { redirect_to(@game,
           :notice => "Game description was successfully updated.") }
       end
+    rescue ActiveRecord::NotNullViolation
+      format.html { redirect_to(@game,
+          :alert => "Game name was not updated."
+      )
+    }
     end
   end
 
