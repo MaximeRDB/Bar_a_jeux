2f9bbb2992f78f0f19b3e77e0e4858da08b376da
Maxime Rollinde de Beaumont <maximerdb@hotmail.fr>
2024-07-24 16:48:47 +0200

    begin geocoding
    Signed-off-by: Maxime Rollinde de Beaumont <maximerdb@hotmail.fr>

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -8,10 +8,15 @@ class BarsController < ApplicationController
 
   def index
     @bars = Bar.all
+
+    @markers = @bars.geocoded.map do |bar|
+      {
+        lat: bar.latitude,
+        lng: bar.longitude,
+        # info_window_html: render_to_string(partial: "shared/info_window", locals: {bar: bar})
+      }
+    end
   end
-
-
-
 
   private
 
---
+++ config/importmap.rb
@@ -0,0 +1,5 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@3.1.2/dist/mapbox-gl.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
