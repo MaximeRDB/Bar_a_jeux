789000dad59f91a6e85594cc03123dcb5be26fc9
Maxime Rollinde de Beaumont <maximerdb@hotmail.fr>
2024-07-24 13:44:12 +0200

    add validation to bar model
    Signed-off-by: Maxime Rollinde de Beaumont <maximerdb@hotmail.fr>

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,5 +1,7 @@
 class Bar < ApplicationRecord
   has_many :games, through: :bar_games
   has_many :bar_games
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   validates :name, :address, presence: true
 end
